812b9aff3488fb574d7e688c10278bad1519ca6d
Johannes Bauer <joe@johannes-bauer.com>
2020-03-22 13:20:40 -0700

    Implemented capture file
    Want to write specific memory portions to a capture file for later
    evaluation (e.g., crypgrographic keys etc).

--- PyGDB.py
+++ PyGDB.py
@@ -21,18 +21,33 @@
 #	Johannes Bauer <JohannesBauer@gmx.de>
 
 import gdb
+import json
+import os
+import time
+import base64
+
+if "PYGDB_RUNFILE" in os.environ:
+	with open(os.environ["PYGDB_RUNFILE"]) as f:
+		pygdb_config = json.load(f)
+else:
+	pygdb_config = { }
 
 class PyGDBCommand(gdb.Command):
 	_CMD_NAME = None
 	_HELP_PAGE = None
 	_ARGS = None
+	_OPTARGS = [ ]
 
 	def __init__ (self):
 		gdb.Command.__init__(self, self._CMD_NAME, gdb.COMMAND_USER)
 
 	def invoke(self, arg, from_tty):
 		presented_args = arg.split()
-		if len(presented_args) == len(self._ARGS):
+		min_arg_count = len(self._ARGS)
+		max_arg_count = min_arg_count + len(self._OPTARGS)
+		if min_arg_count <= len(presented_args) <= max_arg_count:
+			if len(presented_args) < max_arg_count:
+				presented_args += [ None ] * (max_arg_count - len(presented_args))
 			self.run(*presented_args)
 		else:
 			print("Supplied %d arguments, but %d expected: %s %s" % (len(presented_args), len(self._ARGS), self._CMD_NAME, " ".join(self._ARGS)))
@@ -61,3 +76,30 @@ class HexdumpCommand(PyGDBCommand):
 		length = Tools.to_int(length)
 		data = self._read_memory(start, length)
 		HexDump().dump(data)
+
+@PyGDBCommand.register
+class CaptureMemoryCommand(PyGDBCommand):
+	_CMD_NAME = "capturemem"
+	_HELP_PAGE = "captures memory from 'start' ranging 'length' bytes into the capture JSON file"
+	_ARGS = [ "start", "length" ]
+	_OPTARGS = [ "comment" ]
+
+	def run(self, start, length, comment):
+		length = Tools.to_int(length)
+		data = self._read_memory(start, length)
+		capture = {
+			"ts": time.time(),
+			"symbol": start,
+			"data": base64.b64encode(data).decode("ascii"),
+		}
+		if comment is not None:
+			capture["comment"] = comment
+		capturefile = pygdb_config.get("capture", "capture_file.json")
+		try:
+			with open(capturefile) as f:
+				content = json.load(f)
+		except (FileNotFoundError, json.decoder.JSONDecodeError):
+			content = [ ]
+		content.append(capture)
+		with open(capturefile, "w") as f:
+			json.dump(content, f)
